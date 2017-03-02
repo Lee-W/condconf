68186e07b6c8bbba32997d882ec6e3280e130d8a
LeeW <cl87654321@gmail.com>
2017-03-02 16:41:15 +0800

    Enable sending variables to module global scope
    Other minor change
    * Send List of Strings instead of String as function decorator
    * Add 'or', 'and' as 'any', 'all' alias

--- condconf/cond_function.py
+++ condconf/cond_function.py
@@ -1,29 +1,33 @@
 from typing import List
 
+from .cond import BaseCond
 
-DEFAULT_FUNCTION_TEMPLATE = """{decorators}def {function_name}({function_args}):
-    {pre_process_code}if {true_condition}:
+
+DEFAULT_FUNCTION_TEMPLATE = """{decorators}
+def {function_name}({function_args}):
+    {pre_process_code}
+    if {true_condition}:
         return True
     else:
         return False"""
 
 
 class CondFunction:
-    def __init__(self, func_name: str, condition, *,
+    def __init__(self, func_name: str, condition: BaseCond, *,
                  func_args: List = None,
-                 decorators: str = '',
+                 decorators: List = None,
                  pre_process_code: str = ''):
         self.func_name = func_name
         self.condition = condition
+        self.decorators = decorators or []
         self.func_args = func_args or []
-        self.decorators = decorators
         self.pre_process_code = pre_process_code
 
         self.template = DEFAULT_FUNCTION_TEMPLATE
 
     def generate_code(self):
         template_args = {
-            'decorators': self.decorators,
+            'decorators': self.__join_decorators(self.decorators),
             'function_name': self.func_name,
             'function_args': ', '.join(self.func_args),
             'true_condition': self.condition.generate_code(),
@@ -32,6 +36,13 @@ class CondFunction:
         return self.template.format(
             **template_args
         )
+
+    @staticmethod
+    def __join_decorators(decorators_lst):
+        if decorators_lst:
+            return '@' + '\n@'.join(decorators_lst) + '\n'
+        else:
+            return ''
 
     def __repr__(self):
         return self.generate_code()
--- condconf/cond_generator.py
+++ condconf/cond_generator.py
@@ -23,16 +23,16 @@ def cond_factory(cond_dict, *,
     }
 
     cond_cls = None
-    if cond_type == 'any':
+    if cond_type in ['any', 'or']:
         cond_cls = ContainAnyCond
-    elif cond_type == 'all':
+    elif cond_type in ['all', 'and']:
         cond_cls = ContainAllCond
     elif cond_type == 'match':
         cond_cls = MatchAnyCond
-    elif cond_type == 'complex-all':
+    elif cond_type in ['complex-all', 'complex-and']:
         content = [cond_factory(cond, **args) for cond in content]
         cond_cls = ComplexAllCond
-    elif cond_type == 'complex-any':
+    elif cond_type in ['complex-any', 'complex-or']:
         content = [cond_factory(cond, **args) for cond in content]
         cond_cls = ComplexAnyCond
     else:
@@ -42,26 +42,26 @@ def cond_factory(cond_dict, *,
     return cond_cls(content, var_name=var_name)
 
 
-def cond_func_generator(cond_func_configs, *, template_args=None):
+def cond_func_generator(cond_func_configs, template_args=None, *,
+                        cond_var_name='var'):
     if not template_args:
         template_args = dict()
 
-    var_name = template_args.get('var_name')
-    if not var_name:
-        var_name = 'var'
-    else:
-        template_args.pop('var_name')
-
-    for cond_func_config in cond_func_configs:
-        func_name = cond_func_config['name']
-        cond = cond_factory(cond_func_config['condition'],
-                            var_name=var_name)
+    for func_config in cond_func_configs:
+        func_name = func_config['name']
+        cond = cond_factory(func_config['condition'], var_name=cond_var_name)
         cond_func = CondFunction(func_name, cond, **template_args)
         yield cond_func
 
 
 class CondMeta(type):
-    def __new__(mcs, clsname, supers, classdict, *, cond_funcs):
+    def __new__(mcs, clsname, supers, classdict, *,
+                cond_funcs, external_globals=None):
+
+        if external_globals:
+            for key, value in external_globals.items():
+                globals()[key] = value
+
         for cond_func in cond_funcs:
             func_code = cond_func.generate_code()
             logger.debug(func_code)
