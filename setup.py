047eb0f63a18638d1a8900ebd72cc62d452ed19e
LeeW <cl87654321@gmail.com>
2017-03-07 21:54:58 +0800

    Add setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='condconf',
+    packages=['condconf'],
+    version='0.1',
+    description='Generate condition functions from configuration files.',
+    author='Lee-W',
+    author_email='cl87654321@gmail.com',
+    url='https://github.com/Lee-W/condconf',
+    keywords=['condition', 'config'],
+    classifiers=[]
+)
